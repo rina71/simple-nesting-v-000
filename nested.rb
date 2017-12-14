8b1aca6d586cf5929be440fda9ce11bc921990ff
Yurina Shin <yurinas@gmail.com>
2017-12-14 04:16:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -76,7 +77,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 
+
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 
 			 # :languages => ["LISP", "C"]

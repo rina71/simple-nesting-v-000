0877dc3e34c8e4cb439d74f31f155cef9e671023
Yurina Shin <yurinas@gmail.com>
2017-12-14 04:14:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,8 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+programmer_hash[:grace_hopper][:known_for]
+programmer_hash[:grace_hopper][:languages]
 end
 
 def alan_kay_is_known_for
